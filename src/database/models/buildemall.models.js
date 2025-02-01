a9abd2e0d0589536b019a5774634a354703a5c8c
Adarsh Kushwaha <zerotwoadarsh@gmail.com>
2025-02-01 21:21:29 +0530

    Add BuildEmAll model for team submissions

---
+++ src/database/models/buildemall.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const buildEmAll =  new Schema({
+    teamName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    teamMember1:{
+        type: String,
+        required: true
+    },
+    teamMember1RollNo:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    teamMember2:{
+        type: String,
+        required: true
+    },
+    teamMember2RollNo:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+})
