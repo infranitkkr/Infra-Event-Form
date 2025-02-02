4443e37a6908699b092cc5830cdbf86b9ba5b8db
Adarsh Kushwaha <zerotwoadarsh@gmail.com>
2025-02-02 21:38:35 +0530

    Add Cadpro model for team registration

---
+++ src/database/models/cadpro.models.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const cadproSchema = new Schema({
+    teamName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    number:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    teamLeader:{
+        type: String,
+        required: true
+    },
+    teamLeaderRollNo:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    teamMember2:{
+        type: String,
+        required: true
+    },
+    teamMember2RollNo:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    teamMember3:{
+        type: String,
+    },
+    teamMember3RollNo:{
+        type: Number,
+        unique: true
+    }
+});
